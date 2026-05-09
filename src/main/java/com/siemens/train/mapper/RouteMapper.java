25de549d59fec53a952d720a9945dcc87596d733
Attila Mezei-Rácz <mezeiraczattila@gmail.com>
2026-05-09 13:56:58 +0300

    Add postman test

--- src/main/java/com/siemens/train/mapper/RouteMapper.java
+++ src/main/java/com/siemens/train/mapper/RouteMapper.java
@@ -4,6 +4,7 @@ import com.siemens.train.api.RouteDTO;
 import com.siemens.train.entities.RouteBE;
 import org.springframework.stereotype.Component;
 import java.util.stream.Collectors;
+import java.util.Objects;
 
 @Component
 public class RouteMapper {
@@ -20,6 +21,7 @@ public class RouteMapper {
                 entity.getId(),
                 entity.getName(),
                 entity.getStations().stream()
+                        .filter(Objects::nonNull)
                         .map(stationMapper::toDto)
                         .collect(Collectors.toList())
         );
